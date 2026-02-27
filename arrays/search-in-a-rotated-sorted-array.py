1f4be5c4c0d6fa410508dc5c521b3f82e308aba9
deecodegit <deetoxmail@gmail.com>
2026-02-27 15:47:18 +0530

    day 2

---
+++ arrays/search-in-a-rotated-sorted-array.py
@@ -0,0 +1,26 @@
+def searchRSA(nums, target):
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        if nums[low] <= nums[mid]:
+            if nums[low] <= target < nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:  
+            if nums[mid] < target <= nums[high]:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+    return -1
+
+print(searchRSA([4, 5, 6, 7, 0, 1, 2], 0))
+print(searchRSA([4, 5, 6, 7, 0, 1, 2], 3))
+print(searchRSA([1], 0))
