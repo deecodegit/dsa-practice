936b9704cfc93724b5c5631769909f71ea624151
deecodegit <deetoxmail@gmail.com>
2026-02-27 21:20:33 +0530

    day 2

--- arrays/max-subarray.py
+++ arrays/max-subarray.py
@@ -1,3 +1,4 @@
+##m1
 def max_subarray(nums):
     current_sum = max_sum = nums[0]
 
@@ -9,3 +10,23 @@ def max_subarray(nums):
 print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
 print(max_subarray([1]))
 print(max_subarray([5,4,-1,7,8]))
+
+##m2
+def max_subarray_optimised(nums):
+    current_sum = 0
+    max_sum = float('-inf')
+
+    for i in nums:
+        current_sum += i
+            
+        if current_sum > max_sum:
+            max_sum = current_sum
+            
+        if current_sum < 0:
+            current_sum = 0
+
+    return max_sum
+
+print(max_subarray_optimised([-2,1,-3,4,-1,2,1,-5,4]))
+print(max_subarray_optimised([1]))
+print(max_subarray_optimised([5,4,-1,7,8]))
