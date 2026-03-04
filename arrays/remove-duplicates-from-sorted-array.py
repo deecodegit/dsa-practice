1bfb3f999ea14026a6968ed1d17514229e5b9c48
deecodegit <deetoxmail@gmail.com>
2026-03-04 12:03:05 +0530

    day 4

---
+++ arrays/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,11 @@
+def remove_duplicates(nums):
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[i] != nums[j]:
+            i += 1
+            nums[i] = nums[j]
+    return nums[:i+1] if nums else 0
+
+print(remove_duplicates([1, 1, 2]))
+print(remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+print(remove_duplicates([]))
